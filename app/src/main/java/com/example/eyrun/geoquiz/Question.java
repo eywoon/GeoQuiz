f373e4e95bd00818f2785c64ecf41bb306c9d5ad
eywoon <eyrunthr@gmail.com>
2016-02-28 20:07:44 +0000

    Initial commit

---
+++ app/src/main/java/com/example/eyrun/geoquiz/Question.java
@@ -0,0 +1,15 @@
+package com.example.eyrun.geoquiz;
+
+/**
+ * Created by Eyrun on 09/11/15.
+ */
+public class Question {
+
+    private String questionText;
+    private String questionAnswer;
+
+    public Question(int textResId, boolean answerTrue) {
+      //  mTextResId = textResId;
+       // mAnswerTrue = answerTrue;
+    }
+}
